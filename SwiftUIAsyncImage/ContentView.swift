28fd705b3c642a34d8ab29e75f8ce37a37e05314
Studio Kaori <studiokaori.com@gmail.com>
2022-05-22 09:14:29 +0200

    Add: Async image

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // MARK: - 1. BASIC
+        
+        // showing original size
+        // AsyncImage(url: URL(string: imageURL))
+        
+        // MARK : - 2. SCALE
+        
+        AsyncImage(url: URL(string: imageURL), scale: 3.0)
     }
 }
 
