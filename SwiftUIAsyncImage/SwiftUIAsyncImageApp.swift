1ff641cc58840ebe6852f83f4d5bcef212a2d8ef
Studio Kaori <studiokaori.com@gmail.com>
2022-05-22 09:00:24 +0200

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Kaori Persson on 2022-05-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
